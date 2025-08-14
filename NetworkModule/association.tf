c9ef04793263e5bd880350a891f3ebafaf516ebe
Lavanya-ojha25 <lavanya.ojha2504@gmail.com>
2025-08-14 12:07:00 +0530

    Update association.tf

--- NetworkModule/association.tf
+++ NetworkModule/association.tf
@@ -1,3 +1,4 @@
+#Associate NSG On top of Subnet
 resource "azurerm_subnet_network_security_group_association" "nsg_sub1" {
     count                     = length(var.subnet)
     subnet_id                 = azurerm_subnet.subnet[count.index].id
